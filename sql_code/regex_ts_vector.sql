a2b269e27a57911a2e1f5daf8dd8c7ee72bc6fa9
pipeaesac <mario.meili@live.com>
2016-02-07 20:44:15 +0100

    information retrieval part i finished

--- sql_code/regex_ts_vector.sql
+++ sql_code/regex_ts_vector.sql
@@ -1,49 +1,31 @@
-﻿
 -- Table schema
 CREATE TABLE sometextcorpus (
   id serial PRIMARY KEY,
   body text
 );
 
+-- ILIKE: string pattern matching
 SELECT id, body FROM sometextcorpus
-WHERE body ILIKE '%hour%'
+WHERE body ILIKE '%hour%';
 
+-- ~: followed by regular expression
 SELECT * FROM sometextcorpus
-WHERE body ~ 'hour'
+WHERE body ~ 'hour';
 
-SELECT id, body FROM sometextcorpus
-WHERE body LIKE 'hour%'
+-- to_tsvector parses and normalizes
+SELECT to_tsvector(
+ 'a fat cat sat on a mat and ate fat rats')
+ @@ to_tsquery('cat & rat');
 
-SELECT id, body FROM sometextcorpus
-WHERE body LIKE '%hour'
-
-SELECT 'hours' ILIKE 'Hours';
-
--- Exercise 12.1.2
-SELECT 'a fat cat sat on a mat and ate fat rats'::tsvector @@ 'cat & rat'::tsquery;
-
-SELECT to_tsvector('a fat cat sat on a mat and ate fat rats') @@ to_tsquery('cat & rat');
-
-SELECT to_tsvector('the quick brown fox jumps over the lazy dog.') @@ to_tsquery('foxes');
-
-SELECT to_tsvector('Wie passend, Du kämpfst wie eine Kuh.') @@ to_tsquery('kämpfen');
-
--- Exercise 12.1.3
-SELECT title FROM articles WHERE to_tsvector(body) @@ to_tsquery('titles');
-
-SELECT title FROM articles WHERE to_tsvector(title || ' ' || body) @@ to_tsquery('hoops');
-
--- Exercise 12.1.4
-SELECT title, ts_rank_cd(to_tsvector(body), query) AS rank
-FROM articles, to_tsquery('hoops|(full & useful)') query WHERE query @@ to_tsvector(body)
-ORDER BY rank DESC
-LIMIT 10;
-
-SELECT title, ts_rank_cd(to_tsvector(body), query, 32 /* rank/(rank+1) */ ) AS rank FROM articles, to_tsquery('hoops|(full & useful)') query
+SELECT title FROM articles WHERE to_tsvector(
+ title || ' ' || body) 
+ @@ to_tsquery('hoops');
+ 
+-- ts_rank_cd example
+SELECT title, ts_rank_cd(to_tsvector(body)
+ , query, 32 /* rank/(rank+1) */ ) AS rank
+ FROM articles,
+ to_tsquery('hoops|(full & useful)') query
 WHERE query @@ to_tsvector(body)
 ORDER BY rank DESC
 LIMIT 10;
-
-
-
-
---
+++ sql_code/wild_card_queries.sql
@@ -0,0 +1,5 @@
+SELECT id, body FROM sometextcorpus
+WHERE body LIKE 'hour%';
+
+SELECT id, body FROM sometextcorpus
+WHERE body LIKE '%hour';
