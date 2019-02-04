b561b05da728eec9796217ba969e24c581182642
Michael Blättler <michael.blaettler@innetag.ch>
2019-02-04 13:47:58 +0100

    add/update some stuff after solving last years MEP

--- java_code/neo4j.java
+++ java_code/neo4j.java
@@ -1,3 +1,16 @@
-DROP INDEX IN :Person(name)
-
 MATCH(person:Person) WHERE person.name = 'Andres' RETURN person
+
+// Insert node and relationship:
+CREATE (adam:User { name: 'Adam' }),
+(pernilla:User { name: 'Pernilla' }),
+(david:User { name: 'David'}),
+(adam)-[:FRIEND]->(pernilla),(pernilla)-[:FRIEND]->(david)
+// Insert relationship
+MATCH (u:User { name: "Adam" }), (c:User { name: "David"})
+CREATE (u)-[:FRIEND]->(c)
+// Run Queries:
+MATCH (user:User {name:"Adam"})-[:FRIEND]->(friend)
+WHERE friend.name = "Pernilla"
+RETURN user,friend
+ORDER BY friend.name
+LIMIT 10
